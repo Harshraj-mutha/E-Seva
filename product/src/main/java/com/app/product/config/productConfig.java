8a3593caba88b730f8e81ed1830ff42b5a630e0f
Harshraj-mutha <harshmutha333@gmail.com>
2026-08-18 23:33:14 +0530

    authentication added for product service

--- product/src/main/java/com/app/product/config/productConfig.java
+++ product/src/main/java/com/app/product/config/productConfig.java
@@ -3,13 +3,34 @@ package com.app.product.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.app.product.filter.JwtauthenticationFilter;
+
+import lombok.AllArgsConstructor;
 
 @Configuration
+@AllArgsConstructor
 public class productConfig {
 	
+	private JwtauthenticationFilter jwtAuthFilter;
+	
 	@Bean
-	public ModelMapper getMapper() {
+	 ModelMapper getMapper() {
 		return new ModelMapper();
+	}
+	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http ) {
+		return http.
+			csrf(csrf -> csrf.disable())
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests(req -> req.anyRequest().authenticated())
+			.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+			.build();
 	}
 
 }
---
+++ product/src/main/java/com/app/product/filter/JwtauthenticationFilter.java
@@ -0,0 +1,59 @@
+package com.app.product.filter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.app.product.utils.JwtUtils;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class JwtauthenticationFilter extends OncePerRequestFilter {
+	
+	private JwtUtils jwtUtils;
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		System.out.println("in product jwt filter");
+		String authHeader = request.getHeader("Authorization");
+		if(authHeader ==null || !authHeader.startsWith("Bearer ")) {
+			System.out.println("no token found");
+			return;
+		}
+		System.out.println("token found");
+		String token = authHeader.substring(7);
+		
+		if(!jwtUtils.validateToken(token)) {
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			return;
+		}
+		Claims claims = jwtUtils.getClaims(token);
+        String username = claims.getSubject(); 
+        UsernamePasswordAuthenticationToken authentication =
+            new UsernamePasswordAuthenticationToken(
+                username,
+                null,
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
+            );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        System.out.println("calling next filter");
+		 filterChain.doFilter(request, response);
+		
+		
+	}
+
+}
