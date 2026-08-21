0c66e67f0464b7d9e48087f19c1c0df39f2af23c
Harshraj-mutha <harshmutha333@gmail.com>
2026-08-21 10:50:21 +0530

    Adding Redis caching

--- product/src/main/java/com/app/product/service/ProductService.java
+++ product/src/main/java/com/app/product/service/ProductService.java
@@ -1,8 +1,12 @@
 package com.app.product.service;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,9 +36,20 @@ public class ProductService {
 		return new APIResponseDto("Product added successfully with id : " + AddedProduct.getId());
 	}
 	
+	@Cacheable(value = "products" , key = "#id")
 	public ProductResponseDto getProduct(int id) {
 		Product product = productRepo.getReferenceById(id);
 		return modelMapper.map(product,ProductResponseDto.class);		
+		
+	}
+	
+	@CachePut(value = "products" , key = "#result.id")
+	public ProductResponseDto UpdateProduct(ProductRequestDto productReq) {
+		Product product = productRepo.findByproductName(productReq.getProductName())
+				.orElseThrow(() -> new RuntimeException("Product is not found"));
+		product.setQuantity(productReq.getQuantity());
+		Product updatedProduct = productRepo.save(product);
+		return modelMapper.map(updatedProduct,ProductResponseDto.class);		
 		
 	}
 	
